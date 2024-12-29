7262f890289e203c2214e2e23c18d8ed6e8b00f5
vamsi <vamsi@marmeto.com>
2024-12-29 11:33:43 +0530

    new changes

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -35,16 +35,34 @@ export const NavBar = () => {
     <Router>
       <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
         <Container>
+          <div className="nav-bar-container-card">
           <Navbar.Toggle aria-controls="basic-navbar-nav">
             <span className="navbar-toggler-icon"></span>
           </Navbar.Toggle>
+          <span className="navbar-text navbar-mobile">
+              <div className="social-icon">
+                <a href="https://www.linkedin.com/in/-vamsi/"><img src={navIcon1} alt="" /></a>
+                <a href="mailto:vamsi0263@gmail.com"><img src={navIcon2} alt="mail" /></a>
+                <a href="tel:9108848655"><img src={navIcon3} alt="phone" /></a>
+              </div>
+              <HashLink to='#connect'>
+                <button className="button-animation">
+                  Contact
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                  <span></span>
+                </button>
+              </HashLink>
+            </span>
+          </div>
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="ms-auto">
               <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
               <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
               <Nav.Link href="#project" className={activeLink === 'project' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('project')}>Projects</Nav.Link>
             </Nav>
-            <span className="navbar-text">
+            <span className="navbar-text navbar-desktop">
               <div className="social-icon">
                 <a href="https://www.linkedin.com/in/-vamsi/"><img src={navIcon1} alt="" /></a>
                 <a href="mailto:vamsi0263@gmail.com"><img src={navIcon2} alt="mail" /></a>
